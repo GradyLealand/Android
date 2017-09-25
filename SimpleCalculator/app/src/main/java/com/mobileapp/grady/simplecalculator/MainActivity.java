bc68f9a2b4cd92d2a5ed4d8f645d649e452b647e
kineco42 <kineco@gmail.com>
2017-09-25 10:08:29 -0300

    removed btn private variables

--- SimpleCalculator/app/src/main/java/com/mobileapp/grady/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/mobileapp/grady/simplecalculator/MainActivity.java
@@ -1,3 +1,9 @@
+/*
+*Simple calculator
+*Grady Williams
+*Sept 29, 2017
+*
+ */
 package com.mobileapp.grady.simplecalculator;
 
 import android.support.v7.app.AppCompatActivity;
@@ -14,49 +20,35 @@ public class MainActivity extends AppCompatActivity {
 
     //declare elements
     private EditText displayEditText;
-    private Button btnOne;
-    private Button btnTwo;
-    private Button btnThree;
-    private Button btnFour;
-    private Button btnFive;
-    private Button btnSix;
-    private Button btnSeven;
-    private Button btnEight;
-    private Button btnNine;
-    private Button btnZero;
-    private Button btnDecimal;
-    private Button btnBackSpace;
-    private Button btnClear;
-    private Button btnPluseMinus;
-    private Button btnAdd;
-    private Button btnMinus;
-    private Button btnDivide;
-    private Button btnMultiply;
-    private Button btnEquals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //atatch all button id's
         displayEditText = (EditText) findViewById(R.id.editTextDisplayNums);
-        btnOne = (Button) findViewById(R.id.btnNum1);
-        btnTwo = (Button) findViewById(R.id.btnNum2);
-        btnThree = (Button) findViewById(R.id.btnNum3);
-        btnFour = (Button) findViewById(R.id.btnNum4);
-        btnFive = (Button) findViewById(R.id.btnNum5);
-        btnSix = (Button) findViewById(R.id.btnNum6);
-        btnSeven = (Button) findViewById(R.id.btnNum7);
-        btnEight = (Button) findViewById(R.id.btnNum8);
-        btnNine = (Button) findViewById(R.id.btnNum9);
-        btnZero = (Button) findViewById(R.id.btnNum0);
-        btnDecimal = (Button) findViewById(R.id.btnDecimal);
-        btnBackSpace = (Button) findViewById(R.id.btnBackSpace);
-        btnClear = (Button) findViewById(R.id.btnClear);
-        btnPluseMinus = (Button) findViewById(R.id.btnNegPos);
-        btnAdd = (Button) findViewById(R.id.btnPluse);
-        btnMinus = (Button) findViewById(R.id.btnSubtract);
-        btnDivide = (Button) findViewById(R.id.btnDivide);
-        btnMultiply = (Button) findViewById(R.id.btnMultiply);
-        btnEquals = (Button) findViewById(R.id.btnEquals);
+        //number buttons
+        Button btnOne = (Button) findViewById(R.id.btnNum1);
+        Button btnTwo = (Button) findViewById(R.id.btnNum2);
+        Button btnThree = (Button) findViewById(R.id.btnNum3);
+        Button btnFour = (Button) findViewById(R.id.btnNum4);
+        Button btnFive = (Button) findViewById(R.id.btnNum5);
+        Button btnSix = (Button) findViewById(R.id.btnNum6);
+        Button btnSeven = (Button) findViewById(R.id.btnNum7);
+        Button btnEight = (Button) findViewById(R.id.btnNum8);
+        Button btnNine = (Button) findViewById(R.id.btnNum9);
+        Button btnZero = (Button) findViewById(R.id.btnNum0);
+        //function buttons
+        Button btnDecimal = (Button) findViewById(R.id.btnDecimal);
+        Button btnBackSpace = (Button) findViewById(R.id.btnBackSpace);
+        Button btnClear = (Button) findViewById(R.id.btnClear);
+        Button btnPluseMinus = (Button) findViewById(R.id.btnNegPos);
+        Button btnAdd = (Button) findViewById(R.id.btnPluse);
+        Button btnMinus = (Button) findViewById(R.id.btnSubtract);
+        Button btnDivide = (Button) findViewById(R.id.btnDivide);
+        Button btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        Button btnEquals = (Button) findViewById(R.id.btnEquals);
+
+        //create onClickListeners
     }
 }
