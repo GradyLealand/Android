42eeec7db156a3e010edebd26828f0e36492d91b
kineco42 <kineco@gmail.com>
2017-10-23 10:21:45 -0300

    added a  raw directory and put a file holding the quiz in it

---
+++ StudyQuiz/app/src/main/java/com/mobileapp/grady/studyquiz/QuizFileReader.java
@@ -0,0 +1,11 @@
+package com.mobileapp.grady.studyquiz;
+
+/**
+ * Created by Grady on 2017-10-23.
+ *
+ * a class that will read the quiz file and parse it into a hashmap
+ *
+ */
+
+public class QuizFileReader {
+}
