a9282352d7a1bbe2a55241f403a83f76678aeb21
kineco42 <kineco@gmail.com>
2017-10-24 18:12:28 -0300

    made all quiz buttons and made them repopulate the page with a new question

--- StudyQuiz/app/src/main/java/com/mobileapp/grady/studyquiz/QuizActivity.java
+++ StudyQuiz/app/src/main/java/com/mobileapp/grady/studyquiz/QuizActivity.java
@@ -2,6 +2,8 @@ package com.mobileapp.grady.studyquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 public class QuizActivity extends AppCompatActivity {
 
     private TextView mainDisplay, disAnsA, disAnsB, disAnsC, disAnsD;
+    private Button btnA, btnB, btnC, btnD;
     QuizTools tools = new QuizTools();
     String mySTring;
     String[] quizQuestion = new String[5];
@@ -22,16 +25,59 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         mainDisplay = (TextView)findViewById(R.id.tvDisplay);
+        //atatch elements
         disAnsA = (TextView)findViewById(R.id.tvAnswerA);
         disAnsB = (TextView)findViewById(R.id.tvAnswerB);
         disAnsC = (TextView)findViewById(R.id.tvAnswerC);
         disAnsD = (TextView)findViewById(R.id.tvAnswerD);
+        btnA = (Button)findViewById(R.id.btnAnswerA);
+        btnB = (Button)findViewById(R.id.btnAnswerB);
+        btnC = (Button)findViewById(R.id.btnAnswerC);
+        btnD = (Button)findViewById(R.id.btnAnswerD);
         InputStream is = getResources().openRawResource(R.raw.quiz_questions);
 
+        //read the file and populate the page with the first question
         ArrayList<String> quizArray = tools.readFile(is);
         quizQuestion = tools.makeQuizArray();
         populateQuiz(quizQuestion);
         System.out.print(quizArray);
+
+        //create on click listeners
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get a new question
+                quizQuestion = tools.makeQuizArray();
+                populateQuiz(quizQuestion);
+            }
+        });
+
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get a new question
+                quizQuestion = tools.makeQuizArray();
+                populateQuiz(quizQuestion);
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get a new question
+                quizQuestion = tools.makeQuizArray();
+                populateQuiz(quizQuestion);
+            }
+        });
+
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get a new question
+                quizQuestion = tools.makeQuizArray();
+                populateQuiz(quizQuestion);
+            }
+        });
     }
 
     public void populateQuiz(String[] myArray){
